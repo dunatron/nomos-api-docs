857d6e5fd44669c685d3c401f027019e2f44b606
Heath Dunlop <heath.dunlop.hd@gmail.com>
2018-08-22 17:06:19 +1200

    fnab

--- themes/react-app/src/components/NotesTable.jsx
+++ themes/react-app/src/components/NotesTable.jsx
@@ -19,17 +19,53 @@ import DeleteIcon from "material-ui-icons/Delete"
 import FilterListIcon from "material-ui-icons/FilterList"
 import { lighten } from "material-ui/styles/colorManipulator"
 
+let counter = 0
+// function createData(name, calories, fat, carbs, protein) {
+//   counter += 1
+//   return { id: counter, name, calories, fat, carbs, protein }
+// }
 function createData({ ID, Name, Description, Created, LastEdited }) {
   return { ID, Name, Description, Created, LastEdited }
 }
 
-function getSorting(order, orderBy) {
-  return order === "desc"
-    ? (a, b) => b[orderBy] - a[orderBy]
-    : (a, b) => a[orderBy] - b[orderBy]
+/**
+ * Need to also sort by dates. Pull in Moment for this.
+ * also extract most of this functionality to props.
+ * 1. const rows can pe passed to it surely.
+ * 2. <TableCell>{n.Description}</TableCell> should loop over the fields. Do it all by config
+ * and get all the places
+ */
+
+function desc(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1
+  }
+  return 0
 }
 
-const columnData = [
+function getSorting(order, orderBy) {
+  return order === "desc"
+    ? (a, b) => desc(a, b, orderBy)
+    : (a, b) => -desc(a, b, orderBy)
+}
+
+const rowsOld = [
+  {
+    id: "name",
+    numeric: false,
+    disablePadding: true,
+    label: "Dessert (100g serving)",
+  },
+  { id: "calories", numeric: true, disablePadding: false, label: "Calories" },
+  { id: "fat", numeric: true, disablePadding: false, label: "Fat (g)" },
+  { id: "carbs", numeric: true, disablePadding: false, label: "Carbs (g)" },
+  { id: "protein", numeric: true, disablePadding: false, label: "Protein (g)" },
+]
+
+const rows = [
   {
     id: "Name",
     numeric: false,
@@ -77,22 +113,22 @@ class EnhancedTableHead extends React.Component {
               onChange={onSelectAllClick}
             />
           </TableCell>
-          {columnData.map(column => {
+          {rows.map(row => {
             return (
               <TableCell
-                key={column.id}
-                numeric={column.numeric}
-                padding={column.disablePadding ? "none" : "default"}
-                sortDirection={orderBy === column.id ? order : false}>
+                key={row.id}
+                numeric={row.numeric}
+                padding={row.disablePadding ? "none" : "default"}
+                sortDirection={orderBy === row.id ? order : false}>
                 <Tooltip
                   title="Sort"
-                  placement={column.numeric ? "bottom-end" : "bottom-start"}
+                  placement={row.numeric ? "bottom-end" : "bottom-start"}
                   enterDelay={300}>
                   <TableSortLabel
-                    active={orderBy === column.id}
+                    active={orderBy === row.id}
                     direction={order}
-                    onClick={this.createSortHandler(column.id)}>
-                    {column.label}
+                    onClick={this.createSortHandler(row.id)}>
+                    {row.label}
                   </TableSortLabel>
                 </Tooltip>
               </TableCell>
@@ -203,11 +239,28 @@ class NotesTable extends React.Component {
 
     const notesData = this.props.notes.map(note => createData(note))
 
+    console.log("notesData ", notesData)
+
     this.state = {
       order: "asc",
-      orderBy: "Name",
+      orderBy: "calories",
       selected: [],
       data: notesData,
+      // data: [
+      //   createData("Cupcake", 305, 3.7, 67, 4.3),
+      //   createData("Donut", 452, 25.0, 51, 4.9),
+      //   createData("Eclair", 262, 16.0, 24, 6.0),
+      //   createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+      //   createData("Gingerbread", 356, 16.0, 49, 3.9),
+      //   createData("Honeycomb", 408, 3.2, 87, 6.5),
+      //   createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+      //   createData("Jelly Bean", 375, 0.0, 94, 0.0),
+      //   createData("KitKat", 518, 26.0, 65, 7.0),
+      //   createData("Lollipop", 392, 0.2, 98, 0.0),
+      //   createData("Marshmallow", 318, 0, 81, 2.0),
+      //   createData("Nougat", 360, 19.0, 9, 37.0),
+      //   createData("Oreo", 437, 18.0, 63, 4.0),
+      // ],
       page: 0,
       rowsPerPage: 5,
     }
@@ -303,9 +356,10 @@ class NotesTable extends React.Component {
                       <TableCell component="th" scope="row" padding="none">
                         {n.Name}
                       </TableCell>
-                      <TableCell numeric>{n.Description}</TableCell>
-                      <TableCell numeric>{n.Created}</TableCell>
-                      <TableCell numeric>{n.LastEdited}</TableCell>
+                      <TableCell>{n.Description}</TableCell>
+                      <TableCell>{n.Created}</TableCell>
+                      <TableCell>{n.LastEdited}</TableCell>
+                      {/* <TableCell numeric>{n.Description}</TableCell> */}
                     </TableRow>
                   )
                 })}
@@ -338,7 +392,6 @@ class NotesTable extends React.Component {
 
 NotesTable.propTypes = {
   classes: PropTypes.object.isRequired,
-  notes: PropTypes.arrayOf(PropTypes.object).isRequired,
 }
 
 export default withStyles(styles)(NotesTable)
